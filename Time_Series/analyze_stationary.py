216b01228f2cb03065c86f7df29b6b4a628c2389
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-06-09 19:25:02 -0700

    Add predictions correction to plots

--- Time_Series/analyze_stationary.py
+++ Time_Series/analyze_stationary.py
@@ -170,7 +170,7 @@ def forecast(ts, log_series):
     lag_acf = acf(ts_log_diff, nlags = 20)
     lag_pacf = pacf(ts_log_diff, nlags = 20, method = "ols")
     #plot ACF
-    plt.subplot(211)
+    plt.subplot(221)
     plt.plot(lag_acf)
     plt.axhline(y=0, linestyle="--", color="gray")
     plt.axhline(y=-1.96 / np.sqrt(len(ts_log_diff)), linestyle="--", color="gray") #lower line of confidence interval
@@ -178,7 +178,7 @@ def forecast(ts, log_series):
     plt.title('Autocorrelation Function')
 
     # Plot PACF:
-    plt.subplot(212)
+    plt.subplot(222)
     plt.plot(lag_pacf)
     plt.axhline(y=0, linestyle="--", color="gray")
     plt.axhline(y=-1.96 / np.sqrt(len(ts_log_diff)), linestyle="--", color="gray")
@@ -194,25 +194,26 @@ def forecast(ts, log_series):
 
     #AR model
     res_arima = arima_models(ts_log, 2, 1, 0)
-    plt.subplot(221)
+    # print pd.Series(res_arima.fittedvalues)
+    plt.subplot(223)
     plt.plot(ts_log_diff)
     plt.plot(res_arima.fittedvalues, color='red')
-    plt.title('AR model--RSS: %.4f' % sum((res_arima.fittedvalues - ts_log_diff) ** 2))
+    # plt.title('AR model--RSS: %.4f' % sum((pd.Series(res_arima.fittedvalues) - ts_log_diff) ** 2))
 
     #MA model
     res_ma = arima_models(ts_log, 0, 1, 2)
-    plt.subplot(222)
+    plt.subplot(224)
     plt.plot(ts_log_diff)
     plt.plot(res_ma.fittedvalues, color='red')
-    plt.title('MA model--RSS: %.4f' % sum((res_ma.fittedvalues - ts_log_diff) ** 2))
+    # plt.title('MA model--RSS: %.4f' % sum((res_ma.fittedvalues - ts_log_diff) ** 2))
     plt.plot()
 
     ##Combined model
     res = arima_models(ts_log, 2, 1, 2)
     plt.plot(ts_log_diff)
     plt.plot(res.fittedvalues, color='red')
-    plt.title('RSS: %.4f' % sum((res.fittedvalues - ts_log_diff) ** 2))
-
+    # plt.title('RSS: %.4f' % sum((res.fittedvalues - ts_log_diff) ** 2))
+    plt.show()
     #Here we can see that the AR and MA models have almost the same RSS but combined is significantly better.
 
     #predicting
@@ -231,7 +232,8 @@ def forecast(ts, log_series):
     predictions_ARIMA = np.exp(predictions_arima_log)
     plt.plot(ts)
     plt.plot(predictions_ARIMA)
-    plt.title('RMSE: %.4f' % np.sqrt(sum((predictions_ARIMA - ts) ** 2) / len(ts)))
+    # plt.title('RMSE: %.4f' % np.sqrt(sum((predictions_ARIMA - ts) ** 2) / len(ts)))
+    plt.show()
 
 def arima_models(ts_log, p, d, q):
     model = ARIMA(ts_log, order = (p, d, q))
@@ -243,9 +245,9 @@ if __name__ == "__main__":
     file_name = "AirPassengers.csv"
     data = read(file_path, file_name)
     # print data
-    stationary(data)
-    stationarity_test = dickey_fuller(data["#Passengers"]) #passing series rather than dataframe
-    print stationarity_test
+    # stationary(data)
+    # stationarity_test = dickey_fuller(data["#Passengers"]) #passing series rather than dataframe
+    # print stationarity_test
     print "Though the variation in standard deviation is small, mean is clearly increasing with time and this is not a " \
           "stationary series. Also, the test statistic is way more than the critical values."
     ts_log = make_stationary(data)
