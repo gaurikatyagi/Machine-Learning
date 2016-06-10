c0ecec24b0a423e15257f2f2cc55aaaa8fbea3db
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-06-09 19:12:06 -0700

    Add prediction

--- Time_Series/analyze_stationary.py
+++ Time_Series/analyze_stationary.py
@@ -5,7 +5,8 @@ from statsmodels.tsa.stattools import adfuller
 import os
 from read_csv import read
 from statsmodels.tsa.seasonal import seasonal_decompose
-
+from statsmodels.tsa.stattools import acf, pacf
+from statsmodels.tsa.arima_model import ARIMA
 
 def stationary(time_series_data):
     #Rolling statistics
@@ -130,7 +131,112 @@ def make_stationary(time_series_data):
     ts_log_decompose.dropna(inplace=True)
     stationary(ts_log_decompose)
     #The Dickey-Fuller test statistic is significantly lower than the 1% critical value. This TS is close to stationary.
+    return ts_log
 
+def forecast(ts, log_series):
+    """
+    make model on the TS after differencing. Having performed the trend and seasonality estimation techniques,
+    there can be two situations:
+    A strictly stationary series with no dependence among the values. This is the easy case wherein we can model the
+    residuals as white noise. But this is very rare.
+    A series with significant dependence among values. In this case we need to use some statistical models like ARIMA to
+    forecast the data.
+
+    The predictors depend on the parameters (p,d,q) of the ARIMA model:
+    Number of AR (Auto-Regressive) terms (p): AR terms are just lags of dependent variable. For instance if p is 5,
+    the predictors for x(t) will be x(t-1)...x(t-5).
+    Number of MA (Moving Average) terms (q): MA terms are lagged forecast errors in prediction equation. For instance
+    if q is 5, the predictors for x(t) will be e(t-1)...e(t-5) where e(i) is the difference between the moving average
+    at ith instant and actual value.
+    Number of Differences (d): These are the number of nonseasonal differences, i.e. in this case we took the first
+    order difference. So either we can pass that variable and put d=0 or pass the original variable and put d=1.
+    Both will generate same results.
+
+    We use two plots to determine these numbers. Lets discuss them first.
+
+    Autocorrelation Function (ACF): It is a measure of the correlation between the the TS with a lagged version of itself.
+    For instance at lag 5, ACF would compare series at time instant 't1'...'t2' with series at instant 't1-5'...'t2-5'
+    (t1-5 and t2 being end points).
+    Partial Autocorrelation Function (PACF): This measures the correlation between the TS with a lagged version of itself
+    but after eliminating the variations already explained by the intervening comparisons. Eg at lag 5, it will check
+    the correlation but remove the effects already explained by lags 1 to 4.
+
+    :param log_series:
+    :return:
+    """
+    #ACF and PACF plots
+    ts_log_diff = ts_log - ts_log.shift()
+    ts_log_diff = ts_log_diff.dropna()
+    lag_acf = acf(ts_log_diff, nlags = 20)
+    lag_pacf = pacf(ts_log_diff, nlags = 20, method = "ols")
+    #plot ACF
+    plt.subplot(211)
+    plt.plot(lag_acf)
+    plt.axhline(y=0, linestyle="--", color="gray")
+    plt.axhline(y=-1.96 / np.sqrt(len(ts_log_diff)), linestyle="--", color="gray") #lower line of confidence interval
+    plt.axhline(y=1.96 / np.sqrt(len(ts_log_diff)), linestyle="--", color="gray") #upper line of confidence interval
+    plt.title('Autocorrelation Function')
+
+    # Plot PACF:
+    plt.subplot(212)
+    plt.plot(lag_pacf)
+    plt.axhline(y=0, linestyle="--", color="gray")
+    plt.axhline(y=-1.96 / np.sqrt(len(ts_log_diff)), linestyle="--", color="gray")
+    plt.axhline(y=1.96 / np.sqrt(len(ts_log_diff)), linestyle="--", color="gray")
+    plt.title('Partial Autocorrelation Function')
+    plt.tight_layout()
+
+    #from these plots, we get p and q:
+    #p - The lag value where the PACF chart crosses the upper confidence interval for the first time. If you notice
+    # closely, in this case p=2.
+    #q - The lag value where the ACF chart crosses the upper confidence interval for the first time. If you notice
+    # closely, in this case q=2.
+
+    #AR model
+    res_arima = arima_models(ts_log, 2, 1, 0)
+    plt.subplot(221)
+    plt.plot(ts_log_diff)
+    plt.plot(res_arima.fittedvalues, color='red')
+    plt.title('AR model--RSS: %.4f' % sum((res_arima.fittedvalues - ts_log_diff) ** 2))
+
+    #MA model
+    res_ma = arima_models(ts_log, 0, 1, 2)
+    plt.subplot(222)
+    plt.plot(ts_log_diff)
+    plt.plot(res_ma.fittedvalues, color='red')
+    plt.title('MA model--RSS: %.4f' % sum((res_ma.fittedvalues - ts_log_diff) ** 2))
+    plt.plot()
+
+    ##Combined model
+    res = arima_models(ts_log, 2, 1, 2)
+    plt.plot(ts_log_diff)
+    plt.plot(res.fittedvalues, color='red')
+    plt.title('RSS: %.4f' % sum((res.fittedvalues - ts_log_diff) ** 2))
+
+    #Here we can see that the AR and MA models have almost the same RSS but combined is significantly better.
+
+    #predicting
+    predictions_diff = pd.Series(res.fittedvalues, copy=True)
+    print predictions_diff.head()
+    #Notice that these start from '1949-02-01' and not the first month; because we took a lag by 1 and first element
+    # doesn't have anything before it to subtract from. The way to convert the differencing to log scale is to add these
+    # differences consecutively to the base number. An easy way to do it is to first determine the cumulative sum at
+    # index and then add it to the base number. The cumulative sum can be found as:
+    predictions_diff_cumsum = predictions_diff.cumsum()
+    #now add them to the base number
+
+    predictions_arima_log = pd.Series(ts_log.ix[0], index = ts_log.index)
+    predictions_arima_log = predictions_arima_log.add(predictions_diff_cumsum, fill_value = 0)
+    #now let us take the exponential to regain original form of series
+    predictions_ARIMA = np.exp(predictions_arima_log)
+    plt.plot(ts)
+    plt.plot(predictions_ARIMA)
+    plt.title('RMSE: %.4f' % np.sqrt(sum((predictions_ARIMA - ts) ** 2) / len(ts)))
+
+def arima_models(ts_log, p, d, q):
+    model = ARIMA(ts_log, order = (p, d, q))
+    results = model.fit(disp = -1)
+    return results
 
 if __name__ == "__main__":
     file_path = os.path.dirname(os.path.abspath(__name__))
@@ -142,5 +248,5 @@ if __name__ == "__main__":
     print stationarity_test
     print "Though the variation in standard deviation is small, mean is clearly increasing with time and this is not a " \
           "stationary series. Also, the test statistic is way more than the critical values."
-    make_stationary(data)
-    
+    ts_log = make_stationary(data)
+    forecast(data["#Passengers"], ts_log)
