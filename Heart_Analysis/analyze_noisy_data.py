1f156366c1eab9afd2c4222e76ea5c719aa40048
gaurikatyagi <gtyagi@dons.usfca.edu>
2016-06-15 17:03:50 -0700

    find rolling mean for data

--- Heart_Analysis/analyze_noisy_data.py
+++ Heart_Analysis/analyze_noisy_data.py
@@ -15,15 +15,33 @@ from scipy.interpolate import interp1d
 import sys
 
 time_measures = {}
+
 def read_data(filename):
+    """
+    This function reads in the csv file as a pandas dataframe
+    :param filename: String variable which contains the file name to be read
+    :return: returns pandas dataframe containing the csv file
+    """
     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Data", filename)
     print "Reading file: %s"%filename
     try:
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(file_path, index_col= 0)
         print "File read successful"
     except:
         print "File does not exist. Exiting..."
         sys.exit()
+    return data
+
+def rolling_mean(data, window_size, frequency):
+    moving_average = pd.rolling_mean(data["hart"], window = window_size*frequency)
+    heart_rate_average = np.mean(data["hart"])
+    moving_average = [heart_rate_average if math.isnan(average) else average for average in moving_average]
+    data["hart_rolling_mean"] = moving_average
+    return data
 
 if __name__ == "__main__":
-    read_data("noisy_data")
+    dataset = read_data("noisy_data.csv")
+    window_size = 0.75
+    frequency = 100
+    dataset_moving_average = rolling_mean(dataset, window_size, frequency)
+    print dataset_moving_average
